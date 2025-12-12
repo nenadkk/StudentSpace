37bb44f2b420a758f824bb4e4ba6ccea27618b54
Sofdb <sofia.deblasi@studenti.unipd.it>
2025-12-12 08:49:23 +0100

    Aggiunto in relazione e db entità immagine annuncio

--- database/studentSpaceDB.sql
+++ database/studentSpaceDB.sql
@@ -1,23 +1,38 @@
+USE sdeblasi;
+
+-- -----------------------------------------------------
+-- UTENTI
+-- -----------------------------------------------------
+
 CREATE TABLE Utente (
     IdUtente INT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(50) NOT NULL,
-    Cognome VARCHAR(50) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL, --minimo 8 caratteri
+    Nome VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL,
+    Cognome VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL,
+    Email VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+    Password VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL, --minimo 8 caratteri
     IdCitta INT, -- non required
     FOREIGN KEY (IdCitta) REFERENCES Citta(IdCitta)
         ON DELETE SET NULL ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Categoria (
     IdCategoria INT AUTO_INCREMENT PRIMARY KEY,
-    NomeCategoria VARCHAR(100) NOT NULL UNIQUE
-);
+    NomeCategoria VARCHAR(100) COLLATE utf8_unicode_ci NOT NULL UNIQUE
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Citta (
     IdCitta INT AUTO_INCREMENT PRIMARY KEY,
     NomeCitta VARCHAR(100) NOT NULL UNIQUE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Annuncio (
     IdAnnuncio INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,7 +48,22 @@ CREATE TABLE Annuncio (
         ON DELETE SET NULL ON UPDATE CASCADE,
     FOREIGN KEY (IdCitta) REFERENCES Citta(IdCitta)
         ON DELETE SET NULL ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
+CREATE TABLE ImmaginiAnnuncio (
+    IdImmagine INT AUTO_INCREMENT PRIMARY KEY,
+    IdAnnuncio INT NOT NULL,
+    Percorso VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
+    AltText VARCHAR(255),
+    Decorativa TINYINT(1) DEFAULT 0,
+    Ordine INT NOT NULL,
+    FOREIGN KEY (IdAnnuncio) REFERENCES Annuncio(IdAnnuncio)
+        ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
 
 CREATE TABLE Affitti (
     IdAnnuncio INT PRIMARY KEY,
@@ -42,7 +72,10 @@ CREATE TABLE Affitti (
     NumeroInquilini INT NOT NULL,
     FOREIGN KEY (IdAnnuncio) REFERENCES Annuncio(IdAnnuncio)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Esperimenti (
     IdAnnuncio INT PRIMARY KEY,
@@ -51,7 +84,10 @@ CREATE TABLE Esperimenti (
     Compenso DECIMAL(4,2) NOT NULL, -- decimale quindi importante controllo con virgola  o punto
     FOREIGN KEY (IdAnnuncio) REFERENCES Annuncio(IdAnnuncio)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Eventi (
     IdAnnuncio INT PRIMARY KEY,
@@ -60,7 +96,10 @@ CREATE TABLE Eventi (
     CostoEntrata DECIMAL(4,2) NOT NULL, -- decimale quindi importante controllo con virgola  o punto
     FOREIGN KEY (IdAnnuncio) REFERENCES Annuncio(IdAnnuncio)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
 
 CREATE TABLE Ripetizioni (
     IdAnnuncio INT PRIMARY KEY,
@@ -69,4 +108,7 @@ CREATE TABLE Ripetizioni (
     PrezzoOrario DECIMAL(3,2) NOT NULL, -- decimale quindi importante controllo con virgola  o punto
     FOREIGN KEY (IdAnnuncio) REFERENCES Annuncio(IdAnnuncio)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_unicode_ci;
+
